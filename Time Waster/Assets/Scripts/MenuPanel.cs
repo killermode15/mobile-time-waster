b2e5ae33ad24feebed146924886ba6a741823b95
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-10-29 18:22:32 +0800

    [+] Added 7Souls RPG UI Pack [+] Added mock up for UI

---
+++ Time Waster/Assets/Scripts/MenuPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPanel : MonoBehaviour
+{
+    [SerializeField] private List<Button> menuButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectButton(Button selectedButton)
+    {
+        foreach (Button button in menuButtons)
+        {
+            button.interactable = true;
+        }
+
+        selectedButton.interactable = false;
+    }
+}

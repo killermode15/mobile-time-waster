5ce8fa2356e2ab44f92cb281814d46b40c45f105
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-10-29 19:00:26 +0800

    [+] Added simple upgrade for tapping [+] Time is now shown properly

--- Time Waster/Assets/Scripts/Helper/ExtensionScript.cs
+++ Time Waster/Assets/Scripts/Helper/ExtensionScript.cs
@@ -9,8 +9,23 @@ public static class ExtensionScript
         return currPhase == phase;
     }
 
-    public static int SecondsToMinutes(this int seconds)
+    public static int ToSeconds(this int seconds)
     {
-        return seconds / 60;
+        return seconds % 60;
+    }
+
+    public static int ToMinutes(this int seconds)
+    {
+        return (seconds / 60) % 60;
+    }
+
+    public static int ToHours(this int seconds)
+    {
+        return ((seconds / 60) / 60) % 60;
+    }
+
+    public static int ToDays(this int seconds)
+    {
+        return (((seconds / 60) / 60) / 24) % 24;
     }
 }
--- Time Waster/Assets/Scripts/TapManager.cs
+++ Time Waster/Assets/Scripts/TapManager.cs
@@ -4,22 +4,57 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+public class UpgradeData
+{
+    public UpgradeData(float _initial, float _costBase, float _count = 0)
+    {
+        initialCost = _initial;
+        costBase = _costBase;
+        currentCount = _count;
+    }
+
+    [SerializeField] private float initialCost = 1;
+    [SerializeField] private float costBase = 5;
+    [SerializeField] private float currentCount = 0;
+
+    public int GetCost()
+    {
+        return Mathf.RoundToInt(initialCost * Mathf.Pow(costBase, currentCount + 1));
+    }
+
+    public void Upgrade()
+    {
+        currentCount++;
+    }
+}
+
 public class TapManager : MonoBehaviour
 {
     [SerializeField] private int tapCount;
     [SerializeField] private int tapValue;
 
+    [SerializeField] private UpgradeData tapUpgrade;
+
+    [SerializeField] private TextMeshProUGUI timeText;
     [SerializeField] private TextMeshPro onTapPrefab;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        tapUpgrade = new UpgradeData(1, 1.07f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeText.text = "Time: " +
+                        tapCount.ToDays() + " D : " + 
+                        tapCount.ToHours() + " H : " + 
+                        tapCount.ToMinutes() + " M : " + 
+                        tapCount.ToSeconds() + " S";
+
         int touchCount = Input.touchCount;
 
         if (touchCount <= 0)
@@ -40,11 +75,19 @@ public class TapManager : MonoBehaviour
                 Vector3 position = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
                 position.z += 10;
                 GameObject text = Instantiate(onTapPrefab, position, Quaternion.identity).gameObject;
-
+                text.GetComponent<TextMeshPro>().text = "+" + tapValue + "s"; 
                 Destroy(text, 1);
             }
 
             tapCount += tapValue;
         }
+    }
+
+    public void UpgradeTap()
+    {
+        tapUpgrade.Upgrade();
+        tapValue = tapUpgrade.GetCost();
+
+        Debug.Log(tapUpgrade.GetCost().ToString());
     }
 }
